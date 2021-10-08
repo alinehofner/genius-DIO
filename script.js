7e19e90138ee623891c5e40fde631d5b0e77fd42
Aline Hofner <alinehofner@gmail.com>
2021-10-08 20:58:01 -0300

    add functions

--- script.js
+++ script.js
@@ -14,5 +14,11 @@ const yellow = document.querySelector('.yellow');
 
 let shuffledOrder = () => {
   let colorOrder = Math.floor(Math.random() * 4);
+  order[order.length] = colorOrder;
+  clickedOrder = [];
+  for (let i in order) {
+    let elementColor = createColorElement(order[i]);
+    lightColor(elementColor, Number(i) + 1);
+  }
 }
 
